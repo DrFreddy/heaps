92b0b0a2b2af038eb2ae895fac26879adfd4f5e4
DrFreddy <fre_rz@hotmail.com>
2015-10-03 11:00:33 -0500

    obtención de la info del paciente

--- controlPacientes.java
+++ controlPacientes.java
@@ -13,13 +13,12 @@
  */
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 
 
 public class controlPacientes {
-	private String linea;
+	private String linea, sintoma;
 	private String[] palabras, abcdario;
 	private ArrayList<String> lineas= new ArrayList<String>();;
 
@@ -37,17 +36,37 @@ public void lecturaPacientes(String texto){
           lineas.add(linea);
           }
           
-          //Obtención del identificador del paciente  
+          //Obtención de los datos del paciente
           for (int i=0; i < lineas.size(); i++){
+          //Nombre del paciente
+          System.out.println(lineas.get(i).substring(0, lineas.get(i).indexOf(",")));
+          //Sintomas
+          palabras=lineas.get(i).substring(lineas.get(i).indexOf(",")+1,lineas.get(i).length()-1).substring(0,lineas.get(i).substring(lineas.get(i).indexOf(",")+1,lineas.get(i).length()-1).indexOf(",")).split(" ");
+    	  for (int j=0; j< palabras.length; j++){
+        	  if (palabras[j].equals("")== true){
+        		
+        	  }
+        	 
+        	  else{
+        	  sintoma= sintoma+palabras[j]+" ";
+        	  sintoma= sintoma.replace("null", "");
+        	  }
+        	    
+          }
+          
+          System.out.println(sintoma);
+          sintoma="";
+        
+          
+          //Identificador
           System.out.println( lineas.get(i).charAt(lineas.get(i).length()-1));
           }
-      
-      
+               
 		}
 		
 		//Cierre del try
         catch(Exception e){ //En caso que no pueda leerse el archivo
-     	   System.err.println( "ARCHIVO NO ENCONTRADO" );
+     	   System.err.println( e );
        } //Cierre del Catch
 	  
 	}
